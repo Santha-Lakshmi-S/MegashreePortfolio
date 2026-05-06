aa8214493523da0a525a21f1a218f0313a67eda5
Santha-Lakshmi-S <145998696+Santha-Lakshmi-S@users.noreply.github.com>
2026-05-06 15:12:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-// Smooth scroll navigation
+// Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href'))
-            .scrollIntoView({
-                behavior: 'smooth'
-            });
+        .scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+// Modal functions
+function openModal() {
+    document.getElementById("projectModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("projectModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    let modal = document.getElementById("projectModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
